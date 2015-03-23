ad9a953c2283f5e9edf5ae06c314a5c266c986ea
MarkV28 <mark_vels@hotmail.com>
2015-03-23 15:48:48 +0100

    time module take 2

--- modules/time.py
+++ modules/time.py
@@ -1,13 +1,11 @@
 import re
 import datetime
 import time
-from client.app_utils import getTimezone
 
 WORDS = ["TIJD", "HOE LAAT"]
 
-def handle(text, mic, profile):
-	tz = getTimezone(profile)
-	now = datetime.datetime.now(tz=tz)
+def handle(text, mic):
+	now = datetime.datetime.now()
 
 	minuten = now.strftime("%M")
 	uur = now.strftime("%I")
@@ -15,11 +13,9 @@ def handle(text, mic, profile):
 
 	response = minuten + " minuten over " + uur
 
-	mic.say("Het is nu %s." % response)
+	#mic.say("Het is nu %s." % response)
 	time.sleep(2)
 
 
 def isValid(text):
 	return bool(re.search(r'\b(hoe laat|tijd)\b', text, reIGNORECASE))
-
-

4026eaeb2ce4b846587fa9be54d37c157c4a0286
Skievar <skievar@gmail.com>
2015-03-23 15:24:53 +0100

    file geupdate
    handle gedifined, isValid gedifined.

--- modules/time.py
+++ modules/time.py
@@ -1,1 +1,25 @@
-WORDS = ["TIJD", "HOELAAT"]
+import re
+import datetime
+import time
+from client.app_utils import getTimezone
+
+WORDS = ["TIJD", "HOE LAAT"]
+
+def handle(text, mic, profile):
+	tz = getTimezone(profile)
+	now = datetime.datetime.now(tz=tz)
+
+	minuten = now.strftime("%M")
+	uur = now.strftime("%I")
+	uur = uur.replace("O", "")
+
+	response = minuten + " minuten over " + uur
+
+	mic.say("Het is nu %s." % response)
+	time.sleep(2)
+
+
+def isValid(text):
+	return bool(re.search(r'\b(hoe laat|tijd)\b', text, reIGNORECASE))
+
+

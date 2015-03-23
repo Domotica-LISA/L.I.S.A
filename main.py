11abbfe6232b8268726170ea955ea0bb9c355a04
MarkV28 <mark_vels@hotmail.com>
2015-03-23 15:42:25 +0100

    briljant idea take 2

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import wit
 import classes.character
 
 if __name__ == '__main__':
-	try:
-		wit.init('cards.pcm.default')
-		lisa_fsm = classes.character.Lisa()
-		while True:
-			startTime = clock()
-			timeInterval = 1
-			while startTime + timeInterval > clock():
-				pass
-			lisa_fsm.Execute()
-			if not lisa_fsm.Exit():
-				break
-	except KeyboardInterrupt:
-		wit.close()
-		print "Bye Bye!"
+	wit.init('cards.pcm.default')
+	lisa_fsm = classes.character.Lisa()
+	while True:
+		startTime = clock()
+		timeInterval = 1
+		while startTime + timeInterval > clock():
+			pass
+		lisa_fsm.Execute()
+		if not lisa_fsm.Exit():
+			break
+	wit.close()
+	print "Bye Bye!"

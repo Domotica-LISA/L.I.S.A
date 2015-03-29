988489e10fa1b52e145666f0879a6e9a40d9295e
MarkV28 <mark_vels@hotmail.com>
2015-03-30 00:52:02 +0200

    add states & text to speech

--- classes/character.py
+++ classes/character.py
@@ -12,18 +12,20 @@ class Lisa(Char):
 		##print(self.brain.modules)
 
 		## STATES
-		self.FSM.AddState("Wait", states.Wait(self.FSM))
+		self.FSM.AddState("Startup", states.Wait(self.FSM))
 		self.FSM.AddState("Scanning", states.Scanning(self.FSM))
 		self.FSM.AddState("Move", states.Move(self.FSM))
 		self.FSM.AddState("Track", states.Track(self.FSM))
+		self.FSM.AddState("Shutdown", states.Track(self.FSM))
 
 		## TRANSITIONS
-		self.FSM.AddTransition("toWait", transitions.Transition("Wait"))
+		self.FSM.AddTransition("toStartup", transitions.Transition("Startup"))
 		self.FSM.AddTransition("toScanning", transitions.Transition("Scanning"))
 		self.FSM.AddTransition("toMove", transitions.Transition("Move"))
 		self.FSM.AddTransition("toTrack", transitions.Transition("Track"))
+		self.FSM.AddTransition("toShutdown", transitions.Transition("Shutdown"))
 
-		self.FSM.SetState("Wait")
+		self.FSM.SetState("Startup")
 
 	def Execute(self):
 		self.FSM.Execute()

badef887e1cbefdd47c3af89fa82978a8d2d5981
MarkV28 <mark_vels@hotmail.com>
2015-03-30 13:36:24 +0200

    double l

--- classes/tts.py
+++ classes/tts.py
@@ -8,7 +8,7 @@ class Mp3TTSEngine():
 	def play_mp3(self, filename):
 		cmd = ['omxplayer', str(filename)]
 		with tempfile.TemporaryFile() as f:
-			subprocess.cal(cmd, stdout=f, strerr=f)
+			subprocess.call(cmd, stdout=f, strerr=f)
 			f.seek(0)
 			output = f.read()
 
@@ -17,7 +17,7 @@ class GoogleTTS(Mp3TTSEngine):
 		self.language = language
 
 	def say(self, phrase):
-		tts = gtts.gTTS(text="phrase", lang='nl')
+		tts = gtts.gTTS(text=phrase, lang=self.language)
 		print(tts)
 		tts.save("output.mp3")
 		self.play_mp3("output.mp3")

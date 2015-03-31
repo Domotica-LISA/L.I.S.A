017917163ff6e9e2543a882c2d3242549c4ef1de
MarkV28 <mark_vels@hotmail.com>
2015-03-31 13:32:53 +0200

    own gtts

--- classes/tts.py
+++ classes/tts.py
@@ -1,8 +1,11 @@
 import os
-import gtts
 from pygame import mixer
+import re
+import sys
+import argparse
 import time
-import requests
+import urllib, urllib2
+
 
 class Mp3TTSEngine(object):
 	def play_mp3(self, filename):
@@ -11,25 +14,58 @@ class Mp3TTSEngine(object):
 		mixer.music.play()
 		while mixer.music.get_busy() == True:
 			continue
+
+	def split_text(input_text, max_length=100):
+		def split_text_rec(input_text, regexps, max_length=max_length):
+			if(len(input_text) <= max_length): return [input_text]
+			#mistakenly passed a string instead of a list
+			if isinstance(regexps, basestring): regexps = [regexps]
+			regexp = regexps.pop(0) if regexps else '(.{%d})' % max_length
+			text_list = re.split(regexp, input_text)
+			combined_text = []
+			#first segment could be >max_length
+			combined_text.extend(split_text_rec(text_list.pop(0), regexps, max_length))
+			for val in text_list:
+				current = combined_text.pop()
+				concat = current + val
+				if(len(concat) <= max_length):
+					combined_text.append(concat)
+				else:
+					combined_text.append(current)
+					#val could be >max_length
+					combined_text.extend(split_text_rec(val, regexps, max_length))
+		return combined_text
+	return split_text_rec(input_text.replace('\n', ''),['([\,|\.|;]+)', '( )'])
+
 	def save(self, savefile, text):
-		with open(savefile, 'wb') as f:
-			for idx, part in enumerate(text):
-				payload = { 'ie' : 'UTF-8','tl' : 'nl','q' : part,'total' : len(text),'idx' : idx,'textlen' : len(part) }
+				urllib.quote(val),
+				len(combined_text),
+				idx)
+			headers = {	"Host": "translate.google.com",
+						"Referer": "http://www.gstatic.com/translate/sound_player2.swf",
+						"User-Agent": 	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) "
+										"AppleWebKit/535.19 (KHTML, like Gecko) "
+										"Chrome/18.0.1025.163 Safari/535.19"
+			}
+			req = urllib2.Request(mp3url, '', headers)
+			sys.stdout.write('.')
+			sys.stdout.flush()
+			if len(val) > 0:
 				try:
-					r = requests.get('http://translate.google.com/translate_tts', params=payload)
-					for chunk in r.iter_content(chunk_size=1024):
-						f.write(chunk)
-				except Exception as e:
-					raise
-		f.close()
+					response = urllib2.urlopen(req)
+					savefile.write(response.read())
+					time.sleep(.5)
+				except urllib2.URLError as e:
+					print ('%s' % e)
+		savefile.close()
+		print('Saved MP3 to %s' % savefile.name)
 
 class GoogleTTS(Mp3TTSEngine):
 	def __init__(self):
 		pass
 
 	def say(self, phrase):
-		tts = gtts.gTTS(text=phrase, lang='nl')
 		print(phrase)
-		self.save("output.mp3", tts.text_parts)
+		self.save("output.mp3", phrase)
 		self.play_mp3("output.mp3")
 		#os.remove("output.mp3")

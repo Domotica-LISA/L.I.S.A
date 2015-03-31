01de8dd040527403e074600b38960beb21eb9ef9
MarkV28 <mark_vels@hotmail.com>
2015-03-31 11:43:21 +0200

    Save close

--- classes/tts.py
+++ classes/tts.py
@@ -10,12 +10,27 @@ class Mp3TTSEngine(object):
 		mixer.music.play()
 		while mixer.music.get_busy() == True:
 			continue
+	def save(self, savefile, text):
+		with open(savefile, 'wb') as f:
+			for idx, part in enumerate(text):
+				payload = { 'ie' : 'UTF-8',
+				'tl' : 'nl',
+				'q' : part,
+				'total' : len(text),
+				'idx' : idx,
+				'textlen' : len(part) }
+				try:
+					r = requests.get(self.GOOGLE_TTS_URL, params=payload)
+					for chunk in r.iter_content(chunk_size=1024):
+						f.write(chunk)
+					f.close()
+				except Exception as e:
+					raise
 
 class GoogleTTS(Mp3TTSEngine):
 	def say(self, phrase):
 		tts = gtts.gTTS(text=phrase, lang='nl')
 		#print(phrase)
-		print tts.save("output.mp3")
-		time.sleep(12)
+		self.save("output.mp3", tts.text_parts)
 		self.play_mp3("output.mp3")
 		#os.remove("output.mp3")

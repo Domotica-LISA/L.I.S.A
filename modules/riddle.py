f864e8c2587441ca41a031370117b848ff42fe86
MarkV28 <mark_vels@hotmail.com>
2015-04-03 22:59:40 +0200

    search smaller word

--- modules/riddle.py
+++ modules/riddle.py
@@ -9,4 +9,4 @@ def handle(text, speaker):
 	time.sleep(2)
 
 def isValid(text):
-	return bool(re.search(r'\bweet je een raadsel\b', text, re.IGNORECASE))
+	return bool(re.search(r'\braadsel\b', text, re.IGNORECASE))

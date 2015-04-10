ee874008f7e5b39d30ba81f1e5ef2769852a97e8
Skievar <skievar@gmail.com>
2015-04-10 09:47:50 +0200

    train module

---
+++ modules/train.py
@@ -0,0 +1,25 @@
+import httplib2
+import re
+from xml.dom import minidom
+
+def handle(text, speaker, profile):
+	http = httplib2.Http(disable_ssl_certificate_validation=True)
+	http.follow_redirects = False
+	http.add_credentials('skievar@gmail.com', '9zMZBc_mLFpII0OMTQct-OMPtf93EEvammhfuDUzQMfLG-NSE1QFDw')
+
+	resp, content = http.request("https://webservices.ns.nl/ns-api-avt?station=Enschede")
+
+	xmldoc = minidom.parseString(content);
+
+	for node in xmldoc.getElementsByTagName('VertrekkendeTrein'):
+	    RitNummer = node.getElementsByTagName("RitNummer")[0].firstChild.data
+	    VertrekTijd = node.getElementsByTagName("VertrekTijd")[0].firstChild.data
+	    EindBestemming = node.getElementsByTagName("EindBestemming")[0].firstChild.data
+	    TreinSoort = node.getElementsByTagName("TreinSoort")[0].firstChild.data
+	    Vervoerder = node.getElementsByTagName("Vervoerder")[0].firstChild.data
+	    VertrekSpoor = node.getElementsByTagName("VertrekSpoor")[0].firstChild.data
+	    
+	    speaker.say("De trein naar " + EindBestemming + " op spoor " + VertrekSpoor + " vertrekt om " + VertrekTijd[11] + VertrekTijd[12] + VertrekTijd[13] + VertrekTijd[14] + VertrekTijd[15])
+
+def isValid(text):
+    return bool(re.search(r'\b(hoelaat vertrekt de trein|hoe laat vertrekt de trein|rijden er nog treinen|hoelaat gaat de trein|hoe laat gaat de trein)\b', text, re.IGNORECASE))    

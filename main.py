0f57cf2641b598cad5f3d6ffb973093c5341f8df
MarkV28 <mark_vels@hotmail.com>
2015-04-17 20:05:28 +0200

    change main no wit api

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 # -*- coding: UTF-8 -*-
 import sys
 from time import clock
-import wit
 from classes import character, path_declarations
 
 sys.path.append(path_declarations.libPath)
 
 if __name__ == '__main__':
 	try:
-		wit.init('cards.pcm.default')
 		lisa_fsm = character.Lisa()
 		while True:
 			startTime = clock()
@@ -17,5 +15,4 @@ if __name__ == '__main__':
 				pass
 			lisa_fsm.execute()
 	except KeyboardInterrupt:
-		wit.close()
 		print "Bye Bye!"

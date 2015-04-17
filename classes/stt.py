f61783eeeb0c2c29dab99c53c995074efc1eac84
MarkV28 <mark_vels@hotmail.com>
2015-04-17 15:44:39 +0200

    requests error

--- classes/stt.py
+++ classes/stt.py
@@ -6,13 +6,11 @@ import requests
 class WitAiSTT(object):
 	def __init__(self, accessToken):
 		self.token = accessToken
-
-	def headers(self):
-		return {'Authorization': 'Bearer %s' % self.token, 'accept': 'application/json', 'Content-Type': 'audio/wav'}
+		self.header = {'Authorization': 'Bearer %s' % self.token, 'accept': 'application/json', 'Content-Type': 'audio/wav'}
 
 	def transcribe(self, fp):
 		data = fp.read()
-		r = requests.post('https://api.wit.ai/speech?v=20150101', data=data, header=self.headers())
+		r = requests.post('https://api.wit.ai/speech?v=20150101', data=data, header=self.header)
 
 		try:
 			r.raise_for_status()

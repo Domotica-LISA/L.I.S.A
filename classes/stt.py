66c6ffdf584f109b3d106c91bee55c6a5b6d5948
MarkV28 <mark_vels@hotmail.com>
2015-04-17 22:19:41 +0200

    Revert "andere aanpak"
    This reverts commit 8be9b67ca116d81d8e3bc6293ea48c49611b776f.

--- classes/stt.py
+++ classes/stt.py
@@ -5,13 +5,11 @@ import requests
 
 class WitAiSTT(object):
 	def __init__(self, accessToken):
-		self.headers = {'Authorization': 'Bearer %s' % accessToken, 'accept': 'application/json', 'Content-Type': 'audio/mpeg3'}
+		self.headers = {'Authorization': 'Bearer %s' % accessToken, 'accept': 'application/json', 'Content-Type': 'audio/wav'}
 
 	def transcribe(self, fp):
-		#data = fp.read()
-		data = open("input.mp3", 'rb')
-		dataRead = data.read()
-		r = requests.post('https://api.wit.ai/speech?v=20150101', data=dataRead, headers=self.headers)
+		data = fp.read()
+		r = requests.post('https://api.wit.ai/speech?v=20150101', data=data, headers=self.headers)
 
 		try:
 			r.raise_for_status()

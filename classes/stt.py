ef77cdf8c7e475b1f6a816006308762f96749536
MarkV28 <mark_vels@hotmail.com>
2015-04-17 22:40:15 +0200

    blah

--- classes/stt.py
+++ classes/stt.py
@@ -4,11 +4,11 @@ import requests
 
 class WitAiSTT(object):
 	def __init__(self, accessToken):
-		self.headers = {'Authorization': 'Bearer %s' % accessToken, 'accept': 'application/json', 'Content-Type': 'audio/wav'}
+		self.headers = {'Authorization': 'Bearer VFE6BB6YN52KM6OKAHHN3SKQETGS53YS', 'accept': 'application/json', 'Content-Type': 'audio/wav'}
 
 	def transcribe(self, fp):
 		data = fp.read()
-		r = requests.post('https://api.wit.ai/speech?v=20150101', data=data)
+		r = requests.post('https://api.wit.ai/speech?v=20150101', data=data, headers=self.headers)
 
 		try:
 			r.raise_for_status()

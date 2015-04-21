8d855619d89e6c0187bba2934b6ddf1c3e828500
Skievar <skievar@gmail.com>
2015-04-21 13:19:04 +0200

    comments

--- classes/states.py
+++ classes/states.py
@@ -12,15 +12,15 @@ from pixy import *
 pixy_init()
 
 #ser = serial.Serial('/dev/ttyACM0', 9600)
-class Blocks(Structure):
-	_fields_ = [ ("type", c_uint),
-		("signature", c_uint),
-		("x", c_uint),
-		("y", c_uint),
-		("width", c_uint),
-		("height", c_uint)]
+#class Blocks(Structure):
+#	_fields_ = [ ("type", c_uint),
+#		("signature", c_uint),
+#		("x", c_uint),
+#		("y", c_uint),
+#		("width", c_uint),
+#		("height", c_uint)]
 
-block = Blocks()
+#block = Blocks()
 
 
 class State(object):

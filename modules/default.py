cd6a80080bc2cf9180f10dade05977a7a219f2a9
Skievar <skievar@gmail.com>
2015-04-22 09:20:14 +0200

    default antwoord

--- modules/default.py
+++ modules/default.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 import time
+import random
 from sys import maxint
 
 WORDS = []
@@ -7,7 +8,8 @@ WORDS = []
 PRIORITY = -(maxint + 1)
 
 def handle(text, speaker, mic, profile):
-	speaker.say("Sorry kan je dat herhalen")
+	defanswer = ('Sorry kan je dat herhalen', 'Dat heb ik niet helemaal begrepen', 'Wat was je vraag precies', 'Ik heb je niet verstaan, wat zei je')
+	speaker.say(random.choice(defanswer))
 	time.sleep(2)
 
 def is_valid(text):

5d8dfc4cf2cb7b2e439b824f135e731bdb69c72d
Skievar <skievar@gmail.com>
2015-04-22 14:35:36 +0200

    time

--- modules/time.py
+++ modules/time.py
@@ -7,7 +7,7 @@ WORDS = ["TIJD", "HOE LAAT", "HOELAAT" "IS", "HET", "WAT", "IS", "DE"]
 
 def handle(text, speaker, mic, profile):
 	now = datetime.time.now()
-	response = now.hour() + now.minute()
+	response = now.time.hour() + now.time.minute()
 	speaker.say("Het is nu %s" % response)
 	time.sleep(2)
 

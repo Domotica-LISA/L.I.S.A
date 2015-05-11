481f73645327120e093334aa04345ff7b3d5ffd5
Skievar <skievar@gmail.com>
2015-05-11 09:45:17 +0200

    hoi werkt dit

--- modules/default.py
+++ modules/default.py
@@ -8,7 +8,6 @@ WORDS = []
 PRIORITY = -(maxint + 1)
 
 def handle(text, speaker, mic, profile):
-	print 'hoi werkt dit?'
 	defanswer = ('Sorry kan je dat herhalen', 'Dat heb ik niet helemaal begrepen', 'Wat was je vraag precies', 'Ik heb je niet verstaan, wat zei je')
 	speaker.say(random.choice(defanswer))
 	time.sleep(2)

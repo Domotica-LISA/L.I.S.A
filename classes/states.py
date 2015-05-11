4ca60961b8d50dc54c25039ecd9790f8986e5c10
MarkV28 <mark_vels@hotmail.com>
2015-05-11 15:11:03 +0200

    jeffrey

--- classes/states.py
+++ classes/states.py
@@ -133,7 +133,7 @@ class Track(State):
 		
 	def enter(self):
 		print "Start Tracking"
-		self.brain.speaker.say("Hoi Mark, waarmee kan ik je helpen?")
+		self.brain.speaker.say("Hoi Jeffrey, waarmee kan ik je helpen?")
 
 	def execute(self):
 		print "Tracking"

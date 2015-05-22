693e38bf9a4047e207018fd429b2ddaf9b613832
MarkV28 <mark_vels@hotmail.com>
2015-05-22 23:35:05 +0200

    change servopos

--- classes/myservo.py
+++ classes/myservo.py
@@ -1,17 +1,17 @@
 # -*- coding: UTF-8 -*-
 
 servoStoragePos = { "basePos": 25,
-					"rotationPos": 100,
+					"rotationPos": 110,
 					"headPos": 130}
 
 servoPos = {"basePos": 25,
-			"rotationPos": 100,
+			"rotationPos": 110,
 			"headPos": 130}
 
 servoMinPos = { "basePos": 25,
-				"rotationPos": 85,
+				"rotationPos": 90,
 				"headPos": 30}
 
 servoMaxPos = { "basePos": 160,
-				"rotationPos": 115,
+				"rotationPos": 130,
 				"headPos": 130}

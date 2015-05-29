f90e479f905c43b80f480ae40b27152d3b11e739
MarkV28 <mark_vels@hotmail.com>
2015-05-29 13:46:40 +0200

    motion

--- classes/mythread.py
+++ classes/mythread.py
@@ -16,47 +16,43 @@ class ColorCodeThread(threading.Thread):
 	def run(self):
 		
 		while 1:
-			center = self.get_center_stick()
-			print center
+			center = {'x': 0, 'y': 0}
 
-			if center['x'] > 200 and center['x'] < 285: 
-				print "rotate left"
-				#myservo.servoPos['rotationPos'] -= 1
-			elif center['x'] > 0 and center['x'] < 200: 
-				print "base left"
-				#myservo.servoPos['basePos'] -= 1
-			elif center['x'] > 355 and center['x'] < 440:
-				print "rotate right"
-				#myservo.servoPos['rotationPos'] += 1
-			elif center['x'] > 440 and center['x'] < 640:
-				print "base right"
-				#myservo.servoPos['basePos'] += 1
-			else:
-				#print "deadzone x"
-				pass
+			count = pixy_get_blocks(1, block)
+			if count > 0:
+				center['x'] = block.x + (block.width / 2)
+				center['y'] = block.y + (block.height / 2)
 
-			if center['y'] > 0 and center['y'] < 175:
-				print "head up"
-				#myservo.servoPos['headPos'] -= 1
-			elif center['y'] > 225 and center['y'] < 400:
-				print "head down"
-				#myservo.servoPos['headPos'] += 1
-			else:
-				#print "deadzone y"
-				pass
+				if center['x'] > 200 and center['x'] < 285: 
+					print "rotate left"
+					myservo.servoPos['rotationPos'] -= 1
+				elif center['x'] > 0 and center['x'] < 200: 
+					print "base left"
+					myservo.servoPos['basePos'] -= 1
+				elif center['x'] > 355 and center['x'] < 440:
+					print "rotate right"
+					myservo.servoPos['rotationPos'] += 1
+				elif center['x'] > 440 and center['x'] < 640:
+					print "base right"
+					myservo.servoPos['basePos'] += 1
+				else:
+					#print "deadzone x"
+					pass
+
+				if center['y'] > 0 and center['y'] < 175:
+					print "head up"
+					myservo.servoPos['headPos'] -= 1
+				elif center['y'] > 225 and center['y'] < 400:
+					print "head down"
+					myservo.servoPos['headPos'] += 1
+				else:
+					#print "deadzone y"
+					pass
 				
 
 			#print "hoi"
 
-			self.serialServo.write("0, %s, %s, %s" % (myservo.servoPos['basePos'], myservo.servoPos['rotationPos'], myservo.servoPos['headPos']))
-
-	def get_center_stick(self):
-		center = {'x': 0, 'y': 0}
-		pixy_get_blocks(1, block)
-		center['x'] = block.x + (block.width / 2)
-		center['y'] = block.y + (block.height / 2)
-
-		return center
+				self.serialServo.write("0, %s, %s, %s" % (myservo.servoPos['basePos'], myservo.servoPos['rotationPos'], myservo.servoPos['headPos']))
 
 class VoiceThread(threading.Thread):
 	def __init__(self, brain, fSM, serialLed, serialServo):

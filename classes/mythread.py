f6262dc39dd0e06e4dedac5ec56e9f940266255a
MarkV28 <mark_vels@hotmail.com>
2015-05-29 13:49:23 +0200

    +-=

--- classes/mythread.py
+++ classes/mythread.py
@@ -25,26 +25,26 @@ class ColorCodeThread(threading.Thread):
 
 				if center['x'] > 200 and center['x'] < 285: 
 					print "rotate left"
-					myservo.servoPos['rotationPos'] -= 1
+					myservo.servoPos['rotationPos'] = myservo.servoPos['rotationPos'] - 1
 				elif center['x'] > 0 and center['x'] < 200: 
 					print "base left"
-					myservo.servoPos['basePos'] -= 1
+					myservo.servoPos['basePos'] = myservo.servoPos['basePos'] - 1
 				elif center['x'] > 355 and center['x'] < 440:
 					print "rotate right"
-					myservo.servoPos['rotationPos'] += 1
+					myservo.servoPos['rotationPos'] = myservo.servoPos['rotationPos'] + 1
 				elif center['x'] > 440 and center['x'] < 640:
 					print "base right"
-					myservo.servoPos['basePos'] += 1
+					myservo.servoPos['basePos'] = myservo.servoPos['basePos'] + 1
 				else:
 					#print "deadzone x"
 					pass
 
 				if center['y'] > 0 and center['y'] < 175:
 					print "head up"
-					myservo.servoPos['headPos'] -= 1
+					myservo.servoPos['headPos'] = myservo.servoPos['headPos'] - 1
 				elif center['y'] > 225 and center['y'] < 400:
 					print "head down"
-					myservo.servoPos['headPos'] += 1
+					myservo.servoPos['headPos'] = myservo.servoPos['headPos'] + 1
 				else:
 					#print "deadzone y"
 					pass

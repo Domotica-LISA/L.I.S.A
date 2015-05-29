515fb0318ede750ca4c9de2e796b6beab1fbeaf0
Skievar <skievar@gmail.com>
2015-05-29 10:23:40 +0200

    hahaha

--- classes/states.py
+++ classes/states.py
@@ -78,7 +78,7 @@ class Startup(State):
 
 	def exit(self):
 		print "Startup complete"
-		self.brain.speaker.say("Buig voor jullie heerser! Haha.")
+		self.brain.speaker.say("Buig voor jullie heerser.  Hahaha.")
 
 class Scanning(State):
 	def __init__(self, fSM, brain):

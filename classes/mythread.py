315d6a33614a7b6a7386d3fec40ef5be841a0078
MarkV28 <mark_vels@hotmail.com>
2015-05-29 09:11:33 +0200

    debug pixy

--- classes/mythread.py
+++ classes/mythread.py
@@ -14,37 +14,36 @@ class ColorCodeThread(threading.Thread):
 		self.serialServo = serialServo
 
 	def run(self):
-		"""
 		while 1:
 			center = self.get_center_stick()
 
 			if center['x'] > 200 and center['x'] < 285: 
-				#print "rotate left"
-				myservo.servoPos['rotationPos'] -= 1
+				print "rotate left"
+				#myservo.servoPos['rotationPos'] -= 1
 			elif center['x'] > 0 and center['x'] < 200: 
-				#print "base left"
-				myservo.servoPos['basePos'] -= 1
+				print "base left"
+				#myservo.servoPos['basePos'] -= 1
 			elif center['x'] > 355 and center['x'] < 440:
-				#print "rotate right"
-				myservo.servoPos['rotationPos'] += 1
+				print "rotate right"
+				#myservo.servoPos['rotationPos'] += 1
 			elif center['x'] > 440 and center['x'] < 640:
-				#print "base right"
-				myservo.servoPos['basePos'] += 1
+				print "base right"
+				#myservo.servoPos['basePos'] += 1
 			else:
 				#print "deadzone x"
 				pass
 
 			if center['y'] > 0 and center['y'] < 175:
-				#print "head up"
-				myservo.servoPos['headPos'] -= 1
+				print "head up"
+				#myservo.servoPos['headPos'] -= 1
 			elif center['y'] > 225 and center['y'] < 400:
-				#print "head down"
-				myservo.servoPos['headPos'] += 1
+				print "head down"
+				#myservo.servoPos['headPos'] += 1
 			else:
 				#print "deadzone y"
 				pass
-			"""
-		print "hoi"
+
+		#print "hoi"
 
 			#self.serialServo.write("0, %s, %s, %s" % (myservo.servoPos['basePos'], myservo.servoPos['rotationPos'], myservo.servoPos['headPos']))
 
@@ -70,8 +69,6 @@ class VoiceThread(threading.Thread):
 
 			input = self.brain.mic.active_listen()
 			print input
-			self.setRingColor(30, 5, 5)
-			self.setRingColor(5, 30, 5)
 
 			if input is not None:
 				if re.search(r'\b(power down|powerdown)\b', input, re.IGNORECASE):

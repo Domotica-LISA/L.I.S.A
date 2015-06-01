7a4552eb03555133a326ad4ba704d3186753fca8
MarkV28 <mark_vels@hotmail.com>
2015-06-01 13:36:05 +0200

    no join sleep 10 seconds

--- classes/states.py
+++ classes/states.py
@@ -143,16 +143,15 @@ class Track(State):
 		self.voiceThread = mythread.VoiceThread(self.brain, self.fSM, serLed, serServo)
 		self.colorCodeThread = mythread.ColorCodeThread(serServo, servoPos)
 
-		threads = []
-		threads.append(self.voiceThread)
-		threads.append(self.colorCodeThread)
+		#threads = []
+		#threads.append(self.voiceThread)
 
 		self.voiceThread.start()
 		self.colorCodeThread.start()
 
 		time.sleep(10)
-		for t in threads:
-			t.join()
+		#for t in threads:
+			#t.join()
 
 	def exit(self):
 		print "Stop Tracking"

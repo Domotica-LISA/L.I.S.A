d29fbbb3ffa805018d99f07b430ecb441f08b94b
MarkV28 <mark_vels@hotmail.com>
2015-06-01 11:19:18 +0200

    revert no loop

--- classes/mythread.py
+++ classes/mythread.py
@@ -15,41 +15,42 @@ class ColorCodeThread(threading.Thread):
 		self.servoPos = servoPos
 
 	def run(self):
-		center = {'x': 0, 'y': 0}
+		
+		while 1:
+			center = {'x': 0, 'y': 0}
 
-		count = pixy_get_blocks(1, block)
-		if count > 0:
-			center['x'] = block.x + (block.width)
-			center['y'] = block.y + (block.height)
+			count = pixy_get_blocks(1, block)
+			if count > 0:
+				center['x'] = block.x + (block.width)
+				center['y'] = block.y + (block.height)
 
-			if center['x'] > 200 and center['x'] < 285: 
-				print "rotate left"
-				self.servoPos[1] = self.servoPos[1] - 2
-			elif center['x'] > 0 and center['x'] < 200: 
-				print "base left"
-				self.servoPos[0] = self.servoPos[0] - 2
-			elif center['x'] > 355 and center['x'] < 440:
-				print "rotate right"
-				self.servoPos[1] = self.servoPos[1] + 2
-			elif center['x'] > 440 and center['x'] < 640:
-				print "base right"
-				self.servoPos[0] = self.servoPos[0] + 2
-			else:
-				#print "deadzone x"
-				pass
+				if center['x'] > 200 and center['x'] < 285: 
+					print "rotate left"
+					self.servoPos[1] = self.servoPos[1] - 2
+				elif center['x'] > 0 and center['x'] < 200: 
+					print "base left"
+					self.servoPos[0] = self.servoPos[0] - 2
+				elif center['x'] > 355 and center['x'] < 440:
+					print "rotate right"
+					self.servoPos[1] = self.servoPos[1] + 2
+				elif center['x'] > 440 and center['x'] < 640:
+					print "base right"
+					self.servoPos[0] = self.servoPos[0] + 2
+				else:
+					#print "deadzone x"
+					pass
 
-			if center['y'] > 0 and center['y'] < 175:
-				print "head up"
-				self.servoPos[2] = self.servoPos[2] - 2
-			elif center['y'] > 225 and center['y'] < 400:
-				print "head down"
-				self.servoPos[2] = self.servoPos[2] + 2
-			else:
-				#print "deadzone y"
-				pass
-			print center	
-
-			#self.serialServo.write("0,130,110,130")
+				if center['y'] > 0 and center['y'] < 175:
+					print "head up"
+					self.servoPos[2] = self.servoPos[2] - 2
+				elif center['y'] > 225 and center['y'] < 400:
+					print "head down"
+					self.servoPos[2] = self.servoPos[2] + 2
+				else:
+					#print "deadzone y"
+					pass
+				print center	
+			
 			self.serialServo.write("0, %s, %s, %s" % (self.servoPos[0], self.servoPos[1], self.servoPos[2]))
 			time.sleep(1)
 
@@ -62,22 +63,24 @@ class VoiceThread(threading.Thread):
 		self.serialLed = serialLed
 
 	def run(self):
-		self.setRingColor(5, 30, 5)
+		while 1:
 
-		input = self.brain.mic.active_listen()
-		print input
+			self.setRingColor(5, 30, 5)
 
-		if input is not None:
-			if re.search(r'\b(power down|powerdown)\b', input, re.IGNORECASE):
-				self.fSM.to_transition("toShutdown")
-				break
-			elif re.search(r'\b(dankje|tot ziens)\b', input, re.IGNORECASE):
-				self.brain.speaker.say("graag gedaan. Bye Bye")
-				self.fSM.to_transition("toScanning")
-				break
-			else:
-				self.setRingColor(30, 5, 5)
-				self.brain.query(input)
+			input = self.brain.mic.active_listen()
+			print input
+
+			if input is not None:
+				if re.search(r'\b(power down|powerdown)\b', input, re.IGNORECASE):
+					self.fSM.to_transition("toShutdown")
+					break
+				elif re.search(r'\b(dankje|tot ziens)\b', input, re.IGNORECASE):
+					self.brain.speaker.say("graag gedaan. Bye Bye")
+					self.fSM.to_transition("toScanning")
+					break
+				else:
+					self.setRingColor(30, 5, 5)
+					self.brain.query(input)
 
 	def setRingColor(self, red, green, blue):
 		self.serialLed.write("%s, %s, %s" % (red, green, blue))

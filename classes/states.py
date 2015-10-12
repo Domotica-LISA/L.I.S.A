d67d3db333ae93bfbadb41cfb790d8c3026cfee2
MarkV28 <mark_vels@hotmail.com>
2015-10-12 10:07:33 +0200

    Revert "error write"
    This reverts commit de823ad47d451d884de6044d2dadd50f1456780e.

--- classes/states.py
+++ classes/states.py
@@ -59,7 +59,7 @@ class Scanning(State):
 	def execute(self):
 		print "Scanning"
 		input = self.brain.mic.active_listen()
-		sys.stderr.write(input)
+		sys.stdout.write(input)
 		if input is not None:
 			if re.search(self.persona, input, re.IGNORECASE):
 				self.fSM.to_transition("toMove")

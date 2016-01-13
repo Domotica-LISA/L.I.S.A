c1285cecb19e4be3cfe4e6cfce796e19a8d61082
MarkV28 <mark_vels@hotmail.com>
2016-01-13 15:51:16 +0100

    infinite loop

--- demo.py
+++ demo.py
@@ -1,22 +1,12 @@
 # -*- coding: UTF-8 -*-
 
 import serial
-import time
-
-time.sleep(1)
 
 serServo = serial.Serial('/dev/ttyACM0', 9600)
-
-time.sleep(1)
-
 serLed = serial.Serial('/dev/ttyACM1', 9600)
 
 serServo.write("4")
-
-time.sleep(1)
-
 serLed.write("55,38,0")
-
-time.sleep(1)
-
 serServo.write("3")
+while True:
+    pass

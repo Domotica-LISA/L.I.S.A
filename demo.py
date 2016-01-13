3291beea383219f0e97f26acdde57577d09adb5e
MarkV28 <mark_vels@hotmail.com>
2016-01-13 15:20:16 +0100

    delay

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
 
 import serial
+import time
 
 serServo = serial.Serial('/dev/ttyACM0', 9600)
 serLed = serial.Serial('/dev/ttyACM1', 9600)
 
 serServo.write("4")
+time.sleep(2)
 serLed.write("55,38,0")
 serServo.write("3")

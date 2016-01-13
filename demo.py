eca1cd963871b6dfad27fcb32e5c3ca1c2e4f3d7
MarkV28 <mark_vels@hotmail.com>
2016-01-13 15:09:00 +0100

    update demo

--- demo.py
+++ demo.py
@@ -5,8 +5,9 @@ import serial
 serServo = serial.Serial('/dev/ttyACM0', 9600)
 serLed = serial.Serial('/dev/ttyACM1', 9600)
 
-try:
-    serLed.write("55,38,0")
-    serServo.write("3")
-except KeyboardInterrupt:
-    print "Bye Bye!"
+if __name__ == '__main__':
+    try:
+        serLed.write("55,38,0")
+        serServo.write("3")
+    except KeyboardInterrupt:
+        print "Bye Bye!"

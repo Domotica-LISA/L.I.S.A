995c11ebc9b60066fde9efeba5275158647f180a
MarkV28 <mark_vels@hotmail.com>
2016-01-13 15:13:40 +0100

    Update demo

--- demo.py
+++ demo.py
@@ -5,9 +5,6 @@ import serial
 serServo = serial.Serial('/dev/ttyACM0', 9600)
 serLed = serial.Serial('/dev/ttyACM1', 9600)
 
-if __name__ == '__main__':
-    try:
-        serLed.write("55,38,0")
-        serServo.write("3")
-    except KeyboardInterrupt:
-        print "Bye Bye!"
+serLed.write("55,38,0")
+serServo.write("4")
+serServo.write("3")

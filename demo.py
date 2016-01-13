2a96e594aaecd59e1f144cdd344714183b80e8a4
MarkV28 <mark_vels@hotmail.com>
2016-01-13 15:15:34 +0100

    Update demo

--- demo.py
+++ demo.py
@@ -5,6 +5,6 @@ import serial
 serServo = serial.Serial('/dev/ttyACM0', 9600)
 serLed = serial.Serial('/dev/ttyACM1', 9600)
 
-serLed.write("55,38,0")
 serServo.write("4")
+serLed.write("55,38,0")
 serServo.write("3")

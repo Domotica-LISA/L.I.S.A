7bf6278e9d6b0483eff0a481249033c68611bf16
MarkV28 <mark_vels@hotmail.com>
2016-01-14 14:08:21 +0100

    Higher baudrate

--- demo.py
+++ demo.py
@@ -1,20 +1,10 @@
 # -*- coding: UTF-8 -*-
 
 import serial
-from time import clock
 
-serServo = serial.Serial('/dev/ttyACM0', 9600)
-serLed = serial.Serial('/dev/ttyACM1', 9600)
+serServo = serial.Serial('/dev/ttyACM0', 19200)
+serLed = serial.Serial('/dev/ttyACM1', 19200)
 
-startup = True
-
-while True:
-    starttime = clock()
-    timeinterval = 1
-    if startup:
-        serServo.write("4")
-        startup = False
-    while starttime + timeinterval > clock():
-        pass
-    serLed.write("55,38,0")
-    serServo.write("3")
+serServo.write("4")
+serLed.write("55,38,0")
+serServo.write("3")

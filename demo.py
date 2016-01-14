b31fccf830393102d6fbdb25ac1e1ae5540a1da0
MarkV28 <mark_vels@hotmail.com>
2016-01-14 13:30:54 +0100

    Update demo

--- demo.py
+++ demo.py
@@ -1,12 +1,20 @@
 # -*- coding: UTF-8 -*-
 
 import serial
+from time import clock
 
 serServo = serial.Serial('/dev/ttyACM0', 9600)
 serLed = serial.Serial('/dev/ttyACM1', 9600)
 
-serServo.write("4")
-serLed.write("55,38,0")
-serServo.write("3")
+startup = True
+
 while True:
-    pass
+    starttime = clock()
+    timeinterval = 1
+    if startup:
+        serServo.write("4")
+        startup = False
+    while starttime + timeinterval > clock():
+        pass
+    serLed.write("55,38,0")
+    serServo.write("3")

c6457cf95c20932da1c0adc9c85bedc3cc0a11f2
MarkV28 <mark_vels@hotmail.com>
2016-01-14 15:55:34 +0100

    interval

--- demo.py
+++ demo.py
@@ -4,10 +4,13 @@ import serial
 from time import clock
 
 startTime = clock()
-timeInterval = 5
+timeInterval = 4
 
 serServo = serial.Serial('/dev/ttyACM0', 19200)
 serLed = serial.Serial('/dev/ttyACM1', 19200)
+
+while startTime + timeInterval > clock():
+    pass
 
 serServo.write("4")
 while startTime + timeInterval > clock():

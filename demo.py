d37d901b0b95e9d102d487ad00b2705ed1e98e31
MarkV28 <mark_vels@hotmail.com>
2016-01-14 15:37:05 +0100

    Interval wait

--- demo.py
+++ demo.py
@@ -1,10 +1,22 @@
 # -*- coding: UTF-8 -*-
 
 import serial
+from time import clock
 
 serServo = serial.Serial('/dev/ttyACM0', 19200)
 serLed = serial.Serial('/dev/ttyACM1', 19200)
 
+startTime = clock()
+timeInterval = 1
+
+while startTime + timeInterval > clock():
+    pass
 serServo.write("4")
+while startTime + timeInterval > clock():
+    pass
 serLed.write("55,38,0")
+while startTime + timeInterval > clock():
+    pass
 serServo.write("3")
+while startTime + timeInterval > clock():
+    pass

6915bbfee8697bae854e55c31604d0967835ffa4
MarkV28 <mark_vels@hotmail.com>
2016-01-14 15:52:23 +0100

    4 sec delay

--- demo.py
+++ demo.py
@@ -4,22 +4,19 @@ import serial
 from time import clock
 
 startTime = clock()
-timeInterval = 1
+timeInterval = 4
 
-while startTime + timeInterval > clock():
-    pass
 serServo = serial.Serial('/dev/ttyACM0', 19200)
-while startTime + timeInterval > clock():
-    pass
 serLed = serial.Serial('/dev/ttyACM1', 19200)
-while startTime + timeInterval > clock():
-    pass
+
 serServo.write("4")
 while startTime + timeInterval > clock():
     pass
+
 serLed.write("55,38,0")
 while startTime + timeInterval > clock():
     pass
+
 serServo.write("3")
 while startTime + timeInterval > clock():
     pass
